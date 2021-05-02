ce9b3aba53d5b385404d1b5f9282d81950b4987f
ojoksimovic <olivera2391@gmail.com>
2021-05-02 10:42:44 -0400

    update view

--- django_forms/api/models.py
+++ django_forms/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Test(models.Model):
+    username = models.CharField(max_length=20, unique = True)
+    created_at = models.DateTimeField(auto_now_add=True)
--- django_forms/api/views.py
+++ django_forms/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework import generics, status
+from .models import Test
+from .serializers import TestSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import JsonResponse
 
-# Create your views here.
+
+class TestView(generics.ListAPIView):
+    queryset = Test.objects.all()
+    serializer_class = TestSerializer
+
+    def get(self, request, format=None):
+        
+        return Response({'message': 'Room Retrieved!'}, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        print(request.data.get('username'))
+        username = request.data.get('username')
+        # if not self.request.username.exists(self.request.username.username):
+        #     self.request.username.create()
+        return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
