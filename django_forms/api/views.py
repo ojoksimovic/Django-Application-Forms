685648f1bdcdbb1992555eb3bf3c35cc64b5f0e9
olivera <69132868+ojoksimovic@users.noreply.github.com>
2021-05-09 20:24:53 -0400

    Update views.py

--- django_forms/api/views.py
+++ django_forms/api/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render
 from django.shortcuts import render
 from rest_framework import generics, status
-from .models import Test
-from .serializers import TestSerializer
+from .models import Test, Payment_Activation
+from .serializers import TestSerializer, PaymentActivationSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import viewsets
 from django.http import JsonResponse
 
 
-class TestView(generics.ListAPIView):
+class TestView(viewsets.ViewSet):
     queryset = Test.objects.all()
     serializer_class = TestSerializer
 
-    def get(self, request, format=None):
-        
-        return Response({'message': 'Room Retrieved!'}, status=status.HTTP_200_OK)
+    def list(self, request, format=None):
+        dataset = Test.objects.all()
+        serializer = self.serializer_class(dataset, many=True)
+        print(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request, format=None):
+    def create(self, request, format=None):
         # print(request.data.get('username'))
         username = request.data.get('username')
         serializer = self.serializer_class(data=request.data)
@@ -24,3 +27,9 @@ class TestView(generics.ListAPIView):
             user = Test(username=username)
             user.save()
         return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
+
+# class PaymentActivationView(generics.ListAPIView):
+#     queryset = Payment_Activation.objects.all()
+#     serializer_class=PaymentActivationSerializer
+
+#     def get
