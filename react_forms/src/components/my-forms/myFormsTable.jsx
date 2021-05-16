71d1a72d1ad599d51c62e94d42e544b15978e33f
ojoksimovic <olivera2391@gmail.com>
2021-05-16 12:52:06 -0400

    add forms table

---
+++ react_forms/src/components/my-forms/myFormsTable.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { DataGrid } from '@material-ui/data-grid';
+
+const columns = [
+  { field: 'collection', headerName: 'Collection', width: 130 },
+  { field: 'initiator', headerName: 'Initiator', width: 130 },
+  { field: 'academicYear', headerName: 'Academic Year', width: 130 },
+  { field: 'lastModified', headerName: 'Last Modified', width: 130 },
+  { field: 'submitted', headerName: 'Submitted', width: 130 },
+  { field: 'progress', headerName: 'Progress', width: 100 },
+  { field: 'status', headerName: 'Status', width: 130 },
+  { field: 'actions', headerName: 'Actions', width: 130 },
+];
+
+const rows = [
+    { id: 1, collection : 'Award Payment Activation Form', initiator : 'Joksimovic, Olivera', academicYear : '2021-2022', lastModified : 'May 8, 2021, 4:48 PM', submitted : 'May 7, 2021, 3:00 PM', progress : 'Submitted', status : null, actions : null },
+    { id: 2, collection : 'Award Payment Activation Form', initiator : 'Joksimovic, Olivera', academicYear : '2021-2022', lastModified : 'May 8, 2021, 4:48 PM', submitted : 'May 7, 2021, 3:00 PM', progress : 'Submitted', status : null, actions : null },
+    { id: 3, collection : 'Award Payment Activation Form', initiator : 'Joksimovic, Olivera', academicYear : '2021-2022', lastModified : 'May 8, 2021, 4:48 PM', submitted : 'May 7, 2021, 3:00 PM', progress : 'Submitted', status : null, actions : null },
+    { id: 4, collection : 'Award Payment Activation Form', initiator : 'Joksimovic, Olivera', academicYear : '2021-2022', lastModified : 'May 8, 2021, 4:48 PM', submitted : 'May 7, 2021, 3:00 PM', progress : 'Submitted', status : null, actions : null },
+    { id: 5, collection : 'Award Payment Activation Form', initiator : 'Joksimovic, Olivera', academicYear : '2021-2022', lastModified : 'May 8, 2021, 4:48 PM', submitted : 'May 7, 2021, 3:00 PM', progress : 'Submitted', status : null, actions : null },
+    { id: 6, collection : 'Award Payment Activation Form', initiator : 'Joksimovic, Olivera', academicYear : '2021-2022', lastModified : 'May 8, 2021, 4:48 PM', submitted : 'May 7, 2021, 3:00 PM', progress : 'Submitted', status : null, actions : null },
+
+];
+
+
+
+export default function FormsTable() {
+  return (
+    <div style={{ height: 400, width: 1200 }}>
+      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+    </div>
+  );
+}
