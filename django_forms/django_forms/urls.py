5c5e1ab6a20c25186048e0ee8fbc1606a29d9c21
ojoksimovic <olivera2391@gmail.com>
2021-05-24 16:05:45 -0400

    add frontend app to django app

--- django_forms/django_forms/urls.py
+++ django_forms/django_forms/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('users/', include ('users.urls')),
+    path('', include('frontend.urls')),
 ]
