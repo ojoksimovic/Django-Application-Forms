50b72840d5f939e8f1827dc95a6959ad3154342f
ojoksimovic <olivera2391@gmail.com>
2023-02-23 14:15:34 -0500

    reorganize urls

--- django_forms/users/urls.py
+++ django_forms/users/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import ObtainTokenPairWithColorView, CustomUserCreate, CustomUserEdit, HelloWorldView, UserInfoView, LogoutAndBlacklistRefreshTokenForUserView, GoogleProfileInfo, GoogleLoginView
+from .views import ObtainTokenPairWithColorView, CustomUserCreate, CustomUserEdit, HelloWorldView, UserInfoView, LogoutAndBlacklistRefreshTokenForUserView, GoogleProfileInfoView, GoogleLoginView
 
 urlpatterns = [
-    path('user/create/', CustomUserCreate.as_view(), name="create_user"),
-    path('user/edit/', CustomUserEdit.as_view(), name="edit_user"),
+    path('blacklist/', LogoutAndBlacklistRefreshTokenForUserView.as_view(), name='blacklist'),
+    path('google-login/', GoogleLoginView.as_view(), name = 'google_login'),
+    path('google-profile/', GoogleProfileInfoView.as_view(), name = 'google_profile'),
+    path('hello/', HelloWorldView.as_view(), name='hello_world'),
     path('token/obtain/', ObtainTokenPairWithColorView.as_view(), name='token_create'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('hello/', HelloWorldView.as_view(), name='hello_world'),
     path('user-info/', UserInfoView.as_view(), name='user_info'),
-    path('blacklist/', LogoutAndBlacklistRefreshTokenForUserView.as_view(), name='blacklist'),
-    path('google-profile/', GoogleProfileInfo.as_view(), name = 'google_profile'),
-    path('google-login/', GoogleLoginView.as_view(), name = 'google_login')
+    path('user/create/', CustomUserCreate.as_view(), name="create_user"),
+    path('user/edit/', CustomUserEdit.as_view(), name="edit_user")
 ]
