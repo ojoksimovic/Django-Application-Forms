406597392a6d1fba1c95f8d5579a5f79c0f945e2
ojoksimovic <olivera2391@gmail.com>
2023-02-24 13:53:37 -0500

    add backend support for JWT authentication, reorganize API a bit

--- django_forms/users/views.py
+++ django_forms/users/views.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import CustomUser
 import requests
 
@@ -101,29 +102,32 @@ class GoogleProfileInfoView(APIView):
 class GoogleLoginView(APIView):
 
     permission_classes = (permissions.AllowAny,)
+    # add names to user account
 
     def post(self, request, format=None):
         email = request.data["email"]
         external_id = request.data["external_id"]
-        dataset_external_id = CustomUser.objects.filter(external_id = external_id)
+        external_type = request.data["external_type"]
+        dataset_external_id = CustomUser.objects.filter(external_id = external_id, external_type = external_type)
         dataset_email = CustomUser.objects.filter(email = email)
         
         if dataset_external_id:
             # user already exists with external login info, return profile info
-            serializer = self.serializer_class(dataset_external_id, many=True)
-            return Response(data = serializer.data, status=status.HTTP_200_OK)
+            user = CustomUser.objects.get(email = email)
+            serializer = CustomUserSerializer(dataset_external_id, many=True)
+            json = serializer.data
 
         elif dataset_email:
             # user already exists, update CustomUser model to include external ID and Type 
-            user_object = CustomUser.objects.get(email = email)
-            serializer = CustomUserSerializer(user_object, data=request.data, partial=True)
+            user = CustomUser.objects.get(email = email)
+            serializer = CustomUserSerializer(user, data=request.data, partial=True)
             if serializer.is_valid():
                 external_user = serializer.save()
                 if external_user:
                     json = serializer.data
-                    return Response(json, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        
         else:
             # user does not exist, add to CustomUser model
             serializer = CustomUserSerializer(data=request.data, partial= True)
@@ -131,5 +135,13 @@ class GoogleLoginView(APIView):
                 user = serializer.save()
                 if user:
                     json = serializer.data
-                    return Response(json, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+                    
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+
+        return Response({'data': json, 'access': access_token, 'refresh': refresh_token}, status=status.HTTP_200_OK)
+
+            # TO DO: assign username and password for gmail users to allow for internal JWT authentication
