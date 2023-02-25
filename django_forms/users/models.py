e2fc16720e91851be55189903fa61ed8bd13b1df
ojoksimovic <olivera2391@gmail.com>
2023-02-25 14:52:35 -0500

    add note regarding unique email field

--- django_forms/users/models.py
+++ django_forms/users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
+    # append email field in AbstractUser class to include "Unique = True" parameter
     first_name = models.CharField(blank=True, max_length=100)
     last_name = models.CharField(blank=True, max_length=100)
     department = models.CharField(blank=True, max_length=100)
