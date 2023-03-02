acf4ac90238f0911879e096989e47939482c2994
ojoksimovic <olivera2391@gmail.com>
2023-03-02 14:17:33 -0500

    add MSAL config file

---
+++ react_forms/src/components/login/msal_config.jsx
@@ -0,0 +1,21 @@
+export const msalConfig = {
+    auth: {
+      clientId: "7f2463ae-e7a5-4cc7-aa3d-e00ffe487d94",
+      authority: "https://login.microsoftonline.com/dbc2f2dc-6c15-475c-85b4-63eddfb8199e", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+      redirectUri: "http://localhost:3000",
+    },
+    cache: {
+      cacheLocation: "sessionStorage", // This configures where your cache will be stored
+      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    }
+  };
+  
+  // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
+  export const loginRequest = {
+   scopes: ["User.Read"]
+  };
+  
+  // Add the endpoints here for Microsoft Graph API services you'd like to use.
+  export const graphConfig = {
+      graphMeEndpoint: "Enter_the_Graph_Endpoint_Here/v1.0/me"
+  };
