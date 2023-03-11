eaed51ca227f93521094e61974d7c37b099b93df
ojoksimovic <olivera2391@gmail.com>
2023-03-11 13:19:22 -0500

    remove duplicate loginrequest

--- react_forms/src/components/login/credentials.jsx
+++ react_forms/src/components/login/credentials.jsx
@@ -116,7 +116,6 @@ export default function Credentials() {
         })
         .catch((error) => {
           console.log(error);
-          instance.loginPopup(loginRequest);
         });
     }
   };
